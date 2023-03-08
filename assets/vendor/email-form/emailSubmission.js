7605c609bc0ab9beb8f5e7f2d74a511ccc00cf39
Jose Gil Varon <jgvm91@gmail.com>
2023-03-08 10:24:02 -0500

    se incorpora comportamiento de captcha, validacion y envio

--- assets/vendor/email-form/emailSubmission.js
+++ assets/vendor/email-form/emailSubmission.js
@@ -1,42 +1,81 @@
 const formContactData = document.querySelector("#contact-form");
+var recaptcha_response = "";
 
-async function handleSubmit(event) {
-  event.preventDefault();
-  const form = new FormData(event.target);
+function displayError(thisForm, error) {
+  thisForm.querySelector(".loading").classList.remove("d-block");
+  thisForm.querySelector(".error-message").innerHTML = error;
+  thisForm.querySelector(".error-message").classList.add("d-block");
+}
 
-  event.target.querySelector(".loading").classList.add("d-block");
-  event.target.querySelector(".error-message").classList.remove("d-block");
-  event.target.querySelector(".sent-message").classList.remove("d-block");
+function verifyCaptcha(token) {
+  recaptcha_response = token;
+  return recaptcha_response;
+}
 
-  fetch(event.target.action, {
-    method: event.target.method,
-    body: form,
+function emailFormSubmition(thisForm, action, formData, method) {
+  fetch(action, {
+    method: method,
+    body: formData,
     headers: {
       Accept: "application/json",
     },
   })
     .then((response) => {
-      event.target.querySelector(".loading").classList.remove("d-block");
+      thisForm.querySelector(".loading").classList.remove("d-block");
       if (response.ok) {
-        event.target.querySelector(".sent-message").classList.add("active");
-        event.target.querySelector(".sent-message").classList.add("fade-out");
-        formContactData.reset();
+        thisForm.querySelector(".sent-message").classList.add("d-block");
+        thisForm.querySelector(".sent-message").classList.add("fade-out");
+        thisForm.reset();
       } else {
         response.json().then((data) => {
           if (Object.hasOwn(data, "errors")) {
-            //console.log(data);
-            event.target.querySelector(".error-message").innerHTML = data["errors"].map((error)=> `${error.field}: ${error.message}`);
-            event.target.querySelector(".error-message").classList.add("d-block");
+            data["errors"].map((error) =>
+              displayError(thisForm, `${error.field}: ${error.message}`)
+            );
           } else {
-            event.target.querySelector(".error-message").innerHTML = "Oops! There was a problem submitting your form";
-            event.target.querySelector(".error-message").classList.add("d-block");
+            displayError(
+              thisForm,
+              "Oops! There was a problem submitting your form"
+            );
           }
         });
       }
     })
     .catch((error) => {
-      event.target.querySelector(".error-message").innerHTML = error.text();
-      event.target.querySelector(".error-message").classList.add("d-block");
+      displayError(thisForm, error);
     });
+}
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  let thisForm = this;
+  const formData = new FormData(thisForm);
+  let action = thisForm.getAttribute("action");
+  let method = thisForm.getAttribute("method");
+  let recaptchaSiteKey = document
+    .querySelector(".g-recaptcha")
+    .getAttribute("data-sitekey"); // saving the site key from attr div captcha
+
+  if (!action) {
+    // if the form action exist, then it run
+    displayError(thisForm, "The form action property is not set!");
+  }
+  thisForm.querySelector(".loading").classList.add("d-block");
+  thisForm.querySelector(".error-message").classList.remove("d-block");
+  thisForm.querySelector(".sent-message").classList.remove("d-block");
+
+  // if recaptchaSiteKey exist, then do it
+  if (recaptchaSiteKey) {
+    if(typeof grecaptcha !== "undefined"){
+      if (recaptcha_response.length != 0) {
+        emailFormSubmition(thisForm, action, formData, method);
+        //console.log(`token: ${recaptcha_response}`);
+      } else {
+        displayError(thisForm, "reCAPTCHA is Required!");
+      }
+    } else {
+      displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
+    }
+  }
 }
 formContactData.addEventListener("submit", handleSubmit);
